574777ade8e9115f40d81c310d2879815ff2de65
azizedogan <azizedgn.a@outlook.com>
2024-03-16 23:33:36 +0300

    projeler

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian Component */}
+      <Accordian />
+
+      {/* Random Color Component */}
+      <RandomColor />
+
+      {/* Star Rating Component */}
+      <StarRating />
+
+      {/* Image Slider Component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id: '1',
+        question: "React nedir?",
+        answer: "React, açık kaynaklı bir ön uç (front-end) kütüphanesidir. "
+    },
+    {
+        id: '2',
+        question: "React’ın Ana Özellikleri Nelerdir?",
+        answer: "Virtual DOM kullanarak gerçek DOM üzerinde değişiklikleri optimize eder. Server-side rendering işlemini destekler. Tek yönlü veri akışı (Unidirectional data flow) prensibini takip eder. Yeniden kullanılabilir ve birleştirilebilir (reusable/composable) UI bileşenleri kullanır"
+    },
+    {
+        id: '3',
+        question: "JSX Nedir?",
+        answer: "React bileşenlerini oluşturmak için kullanılır."
+    },
+    {
+        id: '4',
+        question: "Element ve Component Arasındaki Farklar Nelerdir?",
+        answer: "Element, ekranda görünmesini istediğiniz düz bir nesnedir. Elementler, başka elementleri içerebilir ve ucuzdur. Component, bir fonksiyon veya sınıf olarak tanımlanabilir. Props alır ve JSX ağacı döndürür."
+    },
+    {
+        id: '5',
+        question: "Bir Bileşenin Durumu Nasıl Güncellenir?",
+        answer: "Bileşenin durumunu güncellemek için setState yöntemini kullanabilirsiniz."
+    }
+];
+
+export default data;
